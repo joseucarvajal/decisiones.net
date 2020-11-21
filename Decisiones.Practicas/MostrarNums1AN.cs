bd36f81b7bda0db157da7b2e103457a36dec1bea
joseucarvajal <joseucarvajal@gmail.com>
2020-11-21 12:41:46 -0500

    Ejercicio de ciclos y menú principal

---
+++ Decisiones.Practicas/MostrarNums1AN.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Decisiones.Practicas
+{
+    public class MostrarNums1AN
+    {
+        public void MostrarConWhile()
+        {
+            int n = PedirNumero();
+            int i = 0;
+            while (i < n)
+            {
+                Console.WriteLine("i="+i);
+                i++;
+            }
+        }
+
+        public void MostrarConFor()
+        {
+            int n = PedirNumero();
+            for(int i=0; i<n; i++)
+            {
+                Console.WriteLine("i="+i);
+            }
+        }
+
+        public int PedirNumero()
+        {
+            Console.WriteLine("Ingrese un número: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            return numero;
+        }
+    }
+}
--- Decisiones.Practicas/Program.cs
+++ Decisiones.Practicas/Program.cs
@@ -6,12 +6,46 @@ namespace Decisiones.Practicas
     {
         static void Main(string[] args)
         {
-            Sumar s1 = new Sumar();
-            s1.SumarYMostrarResultado();            
-            s1.SumarYMostrarResultado();
-            s1.SumarYMostrarResultado();
+            Console.WriteLine("1. Sumar dos números");
+            Console.WriteLine("2. Ciclo con while");
+            Console.WriteLine("3. Ciclo con for");
+            int opcion = int.Parse(Console.ReadLine());
+            switch (opcion)
+            {
+                case 1:
+                    SumarDosNums();
+                    break;
 
+                case 2:
+                    MostrarNumeros1ANWhile();
+                    break;
+
+                default:
+                    MostrarNumeros1ANFor();
+                    break;
+            }
+            
             Console.ReadLine();
+        }
+
+        public static void MostrarNumeros1ANWhile()
+        {
+            MostrarNums1AN m1 = new MostrarNums1AN();
+            m1.MostrarConWhile();
+        }
+
+        public static void MostrarNumeros1ANFor()
+        {
+            MostrarNums1AN m1 = new MostrarNums1AN();
+            m1.MostrarConFor();
+        }
+
+        public static void SumarDosNums()
+        {
+            Sumar s1 = new Sumar();
+            s1.SumarYMostrarResultado();
+            s1.SumarYMostrarResultado();
+            s1.SumarYMostrarResultado();
         }
 
        
